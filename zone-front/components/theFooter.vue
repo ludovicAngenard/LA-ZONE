be2ab0989ccc32ff6d9c85744e6826a38996e366
ludovic <ludovic.angenard@orange.fr>
2021-11-04 10:58:44 +0100

    add footer and add redirection on app-bar

---
+++ zone-front/components/theFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card class="grey darken-3" style="min-height:100px">
+        <a href="/" class="pt-6 d-flex justify-center align-center text-decoration-none">
+            <span class="align-self-end px-3 white--text"  style="text-shadow: 3px 1px 3px rgba(255, 0, 0, 0.77);line-height: 13px;">LA</span>
+            <span class="d-flex align-end">
+                <img style="width:40px;" src="../assets/images/Z_logo.png" alt="the logo">
+                <span class="white--text" style=" text-shadow: 3px 1px 3px rgba(255, 0, 0, 0.77); line-height: 13px;">ONE</span>
+            </span>
+        </a>
+        <div class="pb-6 pt-6 d-flex justify-center">
+            <a href="/conditions" class="text-decoration-none white--text">
+                <small class="px-5">
+                    conditions générale de vente
+                </small>
+            </a>
+            <a href="/issues" class="text-decoration-none white--text">
+                <small  class="px-5">
+                    problème sur un produit ?
+                </small>
+            </a>
+            <a href="/contact" class="text-decoration-none white--text">
+                <small class="px-5">
+                    nous contacter
+                </small>
+            </a>
+            <a href="/legal" class="text-decoration-none white--text">
+                <small class="px-5">
+                    mentions légales
+                </small>
+            </a>
+        </div>
+    </v-card>
+</template>
